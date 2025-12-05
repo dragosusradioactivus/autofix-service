f3b085081b2d1c41758b064c767a4384686d6018
dragosusradioactivus <dragos.poptoma@gmail.com>
2025-12-06 00:38:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("active");
+}
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.toggle('active');
+}
