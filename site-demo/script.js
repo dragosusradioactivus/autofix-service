88f11e96f1fbb8ce6e6f817913bd2ad24d6f77ad
dragosusradioactivus <dragos.poptoma@gmail.com>
2025-12-06 00:34:55 +0200

    Delete site-demo directory

--- site-demo/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    document.getElementById("navMenu").classList.toggle("active");
-}
-function toggleMenu() {
-    const navMenu = document.getElementById('navMenu');
-    navMenu.classList.toggle('active');
-}
